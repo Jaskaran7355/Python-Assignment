0695b4c33288026f51245a4ae9952fb221ede5ab
Jaskaran7355 <115124293+Jaskaran7355@users.noreply.github.com>
2022-10-05 21:54:13 -0400

    Add files via upload

---
+++ name.py
@@ -0,0 +1,7 @@
+name = input('Enter your name    ')
+
+if len(name) <= 1:
+    print("Sorry, username must be longer than one letter.")
+
+elif len(name) > 20:
+    print("Sorry, username must be longer than one letter.")
